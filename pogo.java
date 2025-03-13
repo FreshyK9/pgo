4b472e87d22165152e794ca733052c4a2de448f5
s34679 <s34679@pjwstk.edu.pl>
2025-03-13 09:18:00 +0100

    Poczatek 2

--- pogo.java
+++ pogo.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
 public class pogo {
+    public static int[] filterEvenIndexOddValue(int[] array) {
+for (int i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+       for (int num : array) {
+           if (num % != 2 ){
+               System.out.println(filterEvenIndexOddValue(1,2,3,4,5)
+               );
+               return array;
+           }
+       }
+
+    }
+}
+
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("wats up");
-        int i = 10;
+
+
     }
 }
